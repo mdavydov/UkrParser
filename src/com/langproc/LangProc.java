8516461efea592972b9f89a88df3119fce161d5a
Maksym Davydov <mdavydov@adva-soft.com>
2012-11-06 19:22:39 +0200

    Try to parse sentence

--- src/com/langproc/LangProc.java
+++ src/com/langproc/LangProc.java
@@ -14,13 +14,16 @@ import java.util.regex.Pattern;
 
 class TaggedWord
 {
+	public int m_index;
+	public int m_hypo_index;
 	public String m_word;
 	public String m_base_word;
 	public String m_dict_tags;
 	public java.util.HashSet<String> m_tags = new java.util.HashSet<String>();
 	
-	public TaggedWord(String word, String base_word, String dict_tags)
+	public TaggedWord(int index, String word, String base_word, String dict_tags)
 	{
+		m_index = index;
 		m_word = word;
 		m_base_word = base_word;
 		m_dict_tags = dict_tags;
@@ -34,12 +37,18 @@ class TaggedWord
 	{
 		m_tags.add(tag);
 	}
+	public void setHypotesisIndex(int hyp_index)
+	{
+		m_hypo_index = hyp_index;
+	}
 	
 	public String toString()
 	{
 		StringBuffer b = new StringBuffer(200);
-		b.append(m_word);
+		b.append(m_index);
 		b.append(" ");
+		b.append(m_word);
+		b.append("<-");
 		b.append(m_base_word);
 		b.append(" ");
 		b.append(m_dict_tags);
@@ -60,16 +69,19 @@ class TagRule
 	public java.util.regex.Pattern m_base_pattern;
 	// tags required for the rule
 	public java.util.regex.Pattern m_dict_pattern;
+	// difference between base form and corrected
+	public java.util.regex.Pattern m_diff_pattern;
 	// tags required for the rule
-	public java.util.List<String> m_req_tags = new java.util.LinkedList();
+	public java.util.List<String> m_req_tags = new java.util.LinkedList<String>();
 	// tags to add
-	public java.util.List<String> m_put_tags = new java.util.LinkedList();
+	public java.util.List<String> m_put_tags = new java.util.LinkedList<String>();
 	
-	TagRule(String word_pattern, String base_pattern, String dict_pattern,  String req_tags, String put_tags)
+	TagRule(String word_pattern, String base_pattern, String dict_pattern, String diff_pattern, String req_tags, String put_tags)
 	{
 		m_word_pattern = Pattern.compile(word_pattern, 0);
 		m_base_pattern = Pattern.compile(base_pattern, 0);
 		m_dict_pattern = Pattern.compile(dict_pattern, 0);
+		m_diff_pattern = Pattern.compile(diff_pattern, 0);
 		
 		CharSequenceWordFinder wf1 = new CharSequenceWordFinder(req_tags);
 		while(wf1.hasNext())
@@ -99,9 +111,15 @@ class TagRule
 		}
 		if (has_all) return false;
 		
+		int i=0;
+		int minlen = Math.min(w.m_word.length(), w.m_base_word.length());
+		for(; i<minlen && w.m_word.charAt(i)==w.m_base_word.charAt(i); ++i) {}
+		String diff = w.m_word.substring(i);
+		
 		if (m_word_pattern.matcher(w.m_word).matches()
 				&& m_base_pattern.matcher(w.m_base_word).matches()
-				&& m_dict_pattern.matcher(w.m_dict_tags).matches())
+				&& m_dict_pattern.matcher(w.m_dict_tags).matches()
+				&& m_diff_pattern.matcher(diff).matches() )
 		{
 			for(String s : m_put_tags)
 			{
@@ -116,6 +134,182 @@ class TagRule
 	}
 }
 
+class SentenceWord
+{
+	public int m_index;
+	public java.util.Vector<TaggedWord> m_hypotheses = new java.util.Vector<TaggedWord>();
+	public java.util.TreeMap<String, SentenceWord> m_dependencies = new java.util.TreeMap<String, SentenceWord>();
+	public TaggedWord m_word=null;
+	
+	public SentenceWord(int index) { m_index = index; m_word = null; }
+	
+	public void setClosed(TaggedWord meaning) { m_word = meaning; }
+	public void setOpen() { m_word = null; m_dependencies.clear(); }
+	public void addChild(String role, SentenceWord w)
+	{
+		m_dependencies.put(role, w);
+	}
+	
+	public void addHypothesis(TaggedWord w)
+	{
+		w.setHypotesisIndex(m_hypotheses.size());
+		m_hypotheses.addElement(w);
+	}
+	
+	void findHypothesisWithTags(java.util.List<TaggedWord> out_list, java.util.List<String> tags)
+	{
+		for(TaggedWord t : m_hypotheses)
+		{
+			boolean good = true;
+			for(String tag: tags)
+			{
+				if (!t.hasTag(tag)) { good=false; break; }
+			}
+			if (good) out_list.add(t);
+		}
+	}
+	
+	public void print()
+	{
+		System.out.print(m_index);
+		System.out.print(" ");
+		if (m_word!=null)
+		{
+			System.out.print("* ");
+			System.out.print(m_word);
+		}
+		else
+		{
+			for(TaggedWord t : m_hypotheses)
+			{
+				System.out.print(t);
+				
+				System.out.print(" || ");
+			}
+		}
+	}
+}
+
+class Sentence
+{
+	public java.util.Vector<SentenceWord> m_words = new java.util.Vector<SentenceWord>();
+	
+	public int numWords() { return m_words.size(); }
+	
+	public void addWord(SentenceWord w)
+	{
+		m_words.addElement(w);
+	}
+	
+	SentenceWord sentenceWordAt(int index) { return m_words.elementAt(index); }
+	
+	void findOpenWordsWithTags(java.util.List<TaggedWord> out_list, int index_from, int index_to, java.util.List<String> tags)
+	{	
+		if (index_to >= m_words.size()) index_to = m_words.size()-1;
+		if (index_to < 0) index_to = 0;
+		if (index_from >= m_words.size()) index_from = m_words.size()-1;
+		if (index_from < 0) index_from = 0;
+		
+		int dir = index_to >= index_from ? +1 : -1;
+		index_to += dir;
+		
+		for(int i=index_from; i!=index_to; i+=dir)
+		{
+			SentenceWord sw = m_words.elementAt(i);
+			sw.findHypothesisWithTags(out_list, tags);
+		}
+	}
+	
+	public void print()
+	{
+		for(SentenceWord w:m_words)
+		{
+			w.print();
+			System.out.println();
+		}
+	}
+}
+
+class SentenceProcessor
+{
+	private static java.util.List<String> stringList(String s)
+	{
+		java.util.List<String> sl = new java.util.Vector<String>();
+		CharSequenceWordFinder wf = new CharSequenceWordFinder(s);
+		while(wf.hasNext())
+		{
+			Word w = wf.next();
+			sl.add(w.toString());
+		}
+		return sl;
+	}
+
+	public void parsePredicate(Sentence s, SentenceWord w)
+	{
+		java.util.List<TaggedWord> out_list = new java.util.Vector<TaggedWord>();
+		
+		// search negations
+		s.findOpenWordsWithTags(out_list, w.m_index-1, w.m_index-1, stringList("Neg"));
+		if (out_list.size()!=0)
+		{
+			SentenceWord sw = s.sentenceWordAt(out_list.get(0).m_index);
+			sw.setClosed(out_list.get(0));
+			w.addChild("Neg", sw );
+		}
+		out_list.clear();
+		
+		// search same-level words. !Add this word required tags!!!
+		s.findOpenWordsWithTags(out_list, w.m_index+1, s.numWords(), stringList("Дієсл"));
+		if (out_list.size()!=0)
+		{
+			SentenceWord sw = s.sentenceWordAt(out_list.get(0).m_index);
+			sw.setClosed(out_list.get(0));
+			w.addChild("Sibling", sw );
+		}
+		out_list.clear();
+		
+		// search Subject
+		s.findOpenWordsWithTags(out_list, 0, w.m_index-1, stringList("Імен, c1"));
+		if (out_list.size()!=0)
+		{
+			SentenceWord sw = s.sentenceWordAt(out_list.get(0).m_index);
+			sw.setClosed(out_list.get(0));
+			w.addChild("Subj", sw );
+		}
+		out_list.clear();
+		
+		// search Object
+		s.findOpenWordsWithTags(out_list, w.m_index+1, s.numWords(), stringList("Імен, c4"));
+		if (out_list.size()!=0)
+		{
+			SentenceWord sw = s.sentenceWordAt(out_list.get(0).m_index);
+			sw.setClosed(out_list.get(0));
+			w.addChild("Obj", sw );
+		}
+		out_list.clear();
+	}
+	
+	public void processSentence(Sentence s)
+	{
+		java.util.List<TaggedWord> out_list = new java.util.Vector<TaggedWord>();
+		s.findOpenWordsWithTags(out_list, 0, s.numWords(), stringList("Дієсл"));
+		
+		System.out.println("Search for Main Predicate (the first predicate in the sentence):");
+		for(TaggedWord w : out_list)
+		{
+			// there should be some scoring system and choice :-) 
+			System.out.println(w);
+			s.sentenceWordAt(w.m_index).setClosed(w);
+			
+			parsePredicate(s, s.sentenceWordAt(w.m_index));
+			// parsing
+			s.print();
+			
+			s.sentenceWordAt(w.m_index).setOpen();
+		}
+	}
+}
+
 public class LangProc
 {
 	OpenOfficeSpellDictionary m_dict;
@@ -123,9 +317,10 @@ public class LangProc
 	java.util.HashSet<String> m_prepositions = new java.util.HashSet<String>();
 	java.util.HashSet<String> m_parenthesis_words = new java.util.HashSet<String>();
 	java.util.HashSet<String> m_particles = new java.util.HashSet<String>();
+	java.util.HashSet<String> m_negations = new java.util.HashSet<String>();
 	java.util.HashSet<String> m_conjunction = new java.util.HashSet<String>();
 	
-	java.util.List<TagRule> m_tag_rules = new java.util.LinkedList();
+	java.util.List<TagRule> m_tag_rules = new java.util.LinkedList<TagRule>();
 	
 	static void fillSet(java.util.HashSet<String> set, String s)
 	{
@@ -233,7 +428,7 @@ public class LangProc
 	  
 	  fillSet(m_particles, "ось, осьде, он, от, ото, це, оце");
 	  fillSet(m_particles, "якраз, ледве, просто, прямо, власне, майже, саме");
-	  fillSet(m_particles, "не, ні, ані");
+	  fillSet(m_particles, "ні, ані");
 	  fillSet(m_particles, "тільки, лише, хоч, хоч би, виключно");
 	  fillSet(m_particles, "і, й, та, таки, аж, навіть, вже, ж, же, бо");
 	  fillSet(m_particles, "хай, нехай, бодай, давай");
@@ -243,16 +438,83 @@ public class LangProc
 	  fillSet(m_particles, "мов, мовби, немов, наче, неначе, начебто, ніби, нібито");
 	  fillSet(m_particles, "як, що за тому");
 	  
+	  fillSet(m_negations, "не");
+	  
 	  fillSet(m_conjunction, "і, або, й, та" );
 	  
-	  m_tag_rules.add(new TagRule(".*ого", ".*ий", ".*", "Прикм", "Род, Одн"));
-	  m_tag_rules.add(new TagRule(".*их", ".*ий",  ".*", "Прикм", "Род, Мн"));
-	  m_tag_rules.add(new TagRule(".*", ".*",  ".*[aioe].*", "", "Імен"));
+	  m_tag_rules.add(new TagRule(".*ого", ".*ий", ".*", ".*", "Прикм", "с2, Одн, M, N"));
+	  m_tag_rules.add(new TagRule(".*их", ".*ий",  ".*", ".*","Прикм", "с2, Мн, M, F"));
 	  
-	  m_tag_rules.add(new TagRule(".*", ".*ти",  ".*", "", "Дієсл"));
-	  m_tag_rules.add(new TagRule(".*", ".*ий",  ".*", "", "Прикм"));
-	  m_tag_rules.add(new TagRule(".*", ".*ко|.*но",  ".*", "", "Присл"));
-	  m_tag_rules.add(new TagRule(".*", ".*чи",  ".*", "", "Дієприсл"));
+	  m_tag_rules.add(new TagRule(".*ому", ".*ий",  ".*", ".*", "Прикм", "c3, Одн, M, N"));
+	  m_tag_rules.add(new TagRule(".*ій", ".*ий",  ".*", ".*", "Прикм", "c3, Одн, F"));
+	  m_tag_rules.add(new TagRule(".*им", ".*ий",  ".*", ".*", "Прикм", "c3, Мн, M, N, F"));
+	  
+	  m_tag_rules.add(new TagRule(".*ий", ".*ий",  ".*", ".*", "Прикм", "c4, Одн, M"));
+	  m_tag_rules.add(new TagRule(".*е", ".*ий",  ".*", ".*", "Прикм", "c4, Одн, N"));
+	  m_tag_rules.add(new TagRule(".*у", ".*ий",  ".*", ".*", "Прикм", "c4, Одн, F")); 
+	  m_tag_rules.add(new TagRule(".*их", ".*ий",  ".*", ".*", "Прикм", "c4, Мн, M"));
+	  
+	  m_tag_rules.add(new TagRule(".*им", ".*ий",  ".*", ".*", "Прикм", "c5, Одн, M"));
+	  m_tag_rules.add(new TagRule(".*ими", ".*ий",  ".*", ".*", "Прикм", "c5, Мн, M"));
+	  m_tag_rules.add(new TagRule(".*ім", ".*ий",  ".*", ".*", "Прикм", "c6, Одн, M"));
+	  m_tag_rules.add(new TagRule(".*их", ".*ий",  ".*", ".*", "Прикм", "c6, Мн, M"));
+
+	  m_tag_rules.add(new TagRule(".*ої", ".*ий", ".*", ".*", "Прикм", "с2, Одн, F"));
+//	  m_tag_rules.add(new TagRule(".*их", ".*ий",  ".*", ".*","Прикм", "с2, Мн"));
+	  m_tag_rules.add(new TagRule(".*ому", ".*ий",  ".*", ".*", "Прикм", "c3, Одн, F"));
+	  m_tag_rules.add(new TagRule(".*им", ".*ий",  ".*", ".*", "Прикм", "c3, Мн, F"));
+	  m_tag_rules.add(new TagRule(".*ий", ".*ий",  ".*", ".*", "Прикм", "c4, Одн, F"));
+	  m_tag_rules.add(new TagRule(".*ого", ".*ий",  ".*", ".*", "Прикм", "c4, Одн, F"));
+	  m_tag_rules.add(new TagRule(".*их", ".*ий",  ".*", ".*", "Прикм", "c4, Мн, F"));
+	  m_tag_rules.add(new TagRule(".*им", ".*ий",  ".*", ".*", "Прикм", "c5, Одн, F"));
+	  m_tag_rules.add(new TagRule(".*ими", ".*ий",  ".*", ".*", "Прикм", "c5, Мн, F"));
+	  m_tag_rules.add(new TagRule(".*ім", ".*ий",  ".*", ".*", "Прикм", "c6, Одн, F"));
+	  m_tag_rules.add(new TagRule(".*их", ".*ий",  ".*", ".*", "Прикм", "c6, Мн, F"));
+	  
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "", "Імен", "c1, c4, Одн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "а|у", "Імен", "c2, Одн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "у", "Імен", "c3, Одн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "ом", "Імен", "c5, Одн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "у", "Імен", "c6, Одн, M"));
+	  
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "и", "Імен", "c1, Мн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "ів", "Імен", "c2, Мн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "ам", "Імен", "c3, Мн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "и", "Імен", "c4, Мн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "ами", "Імен", "c5, Мн, M"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  "efg", "ах", "Імен", "c6, Мн, M"));
+	  
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "ю",  "Дієсл", "o1, Одн, Pres"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "єш", "Дієсл", "o2, Одн, Pres"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "є",  "Дієсл", "o3, Одн, Pres"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "ємо",  "Дієсл", "o1, Мн, Pres"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "єте", "Дієсл", "o2, Мн, Pres"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "ють",  "Дієсл", "o3, Мн, Pres"));
+
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "в",  "Дієсл", "o1, o2, o3, Одн, Past"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "ла", "Дієсл", "o3, Одн, Past, F"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "ло", "Дієсл", "o3, Одн, Past, N"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "ли", "Дієсл", "o1, o2, o3, Мн, Past"));
+	  
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "иму",  "Дієсл", "o1, Одн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "имеш", "Дієсл", "o2, Одн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "име",  "Дієсл", "o3, Одн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "емо", "Дієсл", "o1, Мн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "имете", "Дієсл", "o2, Мн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*", "имуть", "Дієсл", "o3, Мн, Ftr"));
+	  
+	  m_tag_rules.add(new TagRule(".*", "бути", ".*", "ду",  "Дієсл", "o1, Одн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", "бути", ".*", "деш", "Дієсл", "o2, Одн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", "бути", ".*", "де",  "Дієсл", "o3, Одн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", "бути", ".*", "демо", "Дієсл", "o1, Мн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", "бути", ".*", "дете", "Дієсл", "o2, Мн, Ftr"));
+	  m_tag_rules.add(new TagRule(".*", "бути", ".*", "дуть", "Дієсл", "o3, Мн, Ftr"));
+
+	  m_tag_rules.add(new TagRule(".*", ".*",  ".*[aioe].*", ".*", "", "Імен"));
+	  m_tag_rules.add(new TagRule(".*", ".*ти",  ".*", ".*", "", "Дієсл"));
+	  m_tag_rules.add(new TagRule(".*", ".*ий",  ".*", ".*", "", "Прикм"));
+	  m_tag_rules.add(new TagRule(".*", ".*ко|.*но",  ".*", ".*", "", "Присл"));
+	  m_tag_rules.add(new TagRule(".*", ".*чи",  ".*", ".*", "", "Дієприсл"));
 	}
 		
 	
@@ -261,41 +523,72 @@ public class LangProc
 	   return Pattern.compile(regexp, flags).matcher(text) ;
 	}
 	
-	private void printForms(String txt)
+	private void addWordForms(Sentence ss, String word)
 	{
-	      List<HEntry> list = m_dict.checkList(txt.toLowerCase());
+		int index = ss.numWords();
+		SentenceWord sw = new SentenceWord(index);
+		
+	      List<HEntry> list = m_dict.checkList(word.toLowerCase());
+	      
+	      if (list.size()==0)
+	      {
+	    	  TaggedWord w = new TaggedWord(index, word, word, "Punct");
+	    	  sw.addHypothesis(w);
+	      }
+	      
 	      for(HEntry s:list)
 	      {
-	    	  TaggedWord w = new TaggedWord(txt.toLowerCase(), s.word, s.astr);
-	    	  if (txt.equals(txt.toUpperCase())) w.addTag("Cap");
-	    	  if (Character.isUpperCase(txt.charAt(0))) w.addTag("StartCap");
-	    	  if (txt.equals(txt.toLowerCase())) w.addTag("Low");
-	    	  if (txt.toLowerCase().equals(s.word)) w.addTag("Base");
+	    	  TaggedWord w = new TaggedWord(index, word.toLowerCase(), s.word, s.astr);
+	    	  
+	    	  if (word.equals(word.toUpperCase())) w.addTag("Cap");
+	    	  if (Character.isUpperCase(word.charAt(0))) w.addTag("StartCap");
+	    	  if (word.equals(word.toLowerCase())) w.addTag("Low");
+	    	  if (word.toLowerCase().equals(s.word)) w.addTag("Base");
 	    	  
 	    	  //System.out.print( "   " + s.word + " " + s.astr + " " );
 
-	    	  if (m_prepositions.contains(s.word)) w.addTag("Прийм");
-	    	  if (m_parenthesis_words.contains(s.word)) w.addTag("Встав");
-	    	  if (m_particles.contains(s.word)) w.addTag("Част");
-	    	  if (m_pronoun.contains(s.word)) w.addTag("Займ");
-	    	  if (m_conjunction.contains(s.word)) w.addTag("Спол");
+	    	  if (m_prepositions.contains(s.word)) w.addTag("Prep");
+	    	  if (m_parenthesis_words.contains(s.word)) w.addTag("Help");
+	    	  if (m_particles.contains(s.word)) w.addTag("Part");
+	    	  if (m_negations.contains(s.word)) w.addTag("Neg");
+	    	  if (m_pronoun.contains(s.word)) w.addTag("Pron");
+	    	  if (m_conjunction.contains(s.word)) w.addTag("Conj");
 	    	  
 	    	  ApplyRules(w);
 	    	  
-	    	  System.out.println(" " + w);
+	    	  sw.addHypothesis(w);
+	    	  
+	    	  System.out.print("|" + w);
 	      }
+	      ss.addWord(sw);
 	}
 
 	private void checkGrammar(String txt)
 	{
-		CharSequenceWordFinder wf = new CharSequenceWordFinder(txt);
+		//CharSequenceWordFinder wf = new CharSequenceWordFinder(txt);
+		CharSequenceWordFinder wf = new CharSequenceWordFinder( Pattern.compile("[АБВГҐДЕЄЖЗІЙИЇКЛМНОПРСТУФХЦЧШЩЬЮЯабвгґдеєжзійиїклмнопрстуфхцчшщьюя'’]+|,|\\.|\\?|!|\"|\'|;|:|\\)|\\(").matcher(txt) );
+		Sentence ss = new Sentence();
+		
+		SentenceProcessor sp = new SentenceProcessor();
+		
 		while(wf.hasNext())
 		{
 			Word w = wf.next();
-			System.out.println(w.toString());
-			printForms(w.toString());
-			
+			if (w.toString().equals("."))
+			{
+				ss.print();
+				sp.processSentence(ss);
+				ss = new Sentence();
+				continue;
+			}
+			System.out.print(w.toString());
+			int s = w.toString().length();
+			for(int i= 20; i>s; --i) System.out.print(" ");
+			addWordForms(ss, w.toString());
+			System.out.println();
 		}
+		ss.print();
+		sp.processSentence(ss);
 	}
 	
   private void test(SpellChecker checker, String txt)
@@ -307,7 +600,7 @@ public class LangProc
     else
     {
       System.out.println("Bad words: " + badWord);
-      List list = checker.getDictionary().getSuggestions(badWord);
+      List<String> list = checker.getDictionary().getSuggestions(badWord);
       List<String> wl = list;
       for (String s : wl)
       {
